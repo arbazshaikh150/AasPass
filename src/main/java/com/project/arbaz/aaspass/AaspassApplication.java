7f264ab5f1cb5d02f8eb8aceb5bd59f5a82444f1
arbazshaikh150 <arbazshaikh1507@gmail.com>
2026-07-17 17:36:04 +0530

    Initialized a AasPaas Application

---
+++ src/main/java/com/project/arbaz/aaspass/AaspassApplication.java
@@ -0,0 +1,12 @@
+package com.project.arbaz.aaspass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AaspassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaspassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/arbaz/aaspass/AaspassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.arbaz.aaspass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaspassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
