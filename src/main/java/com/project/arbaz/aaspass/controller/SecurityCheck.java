4f352d5de89fd52cf66179f43e8dea6832c600cf
arbazshaikh150 <arbazshaikh1507@gmail.com>
2026-07-17 18:17:10 +0530

    Checked the controller without spring security

---
+++ src/main/java/com/project/arbaz/aaspass/controller/SecurityCheck.java
@@ -0,0 +1,21 @@
+package com.project.arbaz.aaspass.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class SecurityCheck {
+    // Now creating a request and response
+    @GetMapping("/request")
+    Map<String, Object> getSecurityContext(HttpServletRequest request) {
+        return Map.of(
+                "method", request.getMethod(),
+                "uri", request.getRequestURI(),
+                "sessionId", request.getRequestedSessionId() != null ? request.getRequestedSessionId() : "Session Required",
+                "Authorization" , request.getHeader("Authorization")
+        );
+    }
+}
