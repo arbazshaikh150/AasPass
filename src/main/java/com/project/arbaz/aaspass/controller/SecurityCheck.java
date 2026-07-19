58f75e371bcc599703e854f30b375e3f24292ded
arbazshaikh150 <arbazshaikh1507@gmail.com>
2026-07-19 18:06:00 +0530

    Added environment variables

--- src/main/java/com/project/arbaz/aaspass/controller/SecurityCheck.java
+++ src/main/java/com/project/arbaz/aaspass/controller/SecurityCheck.java
@@ -5,6 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
+
+/*
+    FLOW :
+        SecurityFilters --> oAuth2 Redirect Google --> then code --> then token
+            --> validation of id token --> loads user info (from google rep)
+            --> creates authentication object --> store in http session
+            --> tomcat sends the jsessionid cookie
+
+    DefaultOidcuser --> oauth2authenticationtoken --> securitycontext --> httpsession
+    Now :
+        Spring loads user info from our database -->
+        save/update user in postgres --> load roles --> create a custom oidcuser with roles
+        --> oauth2authenticationtoken --> securitycontext --> httpsession
+ */
+
 @RestController
 public class SecurityCheck {
     // Now creating a request and response
